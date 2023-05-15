7f52c3f1c13170af11be2718c46758c8e858e2cc
Ashley Mannix <kodraus@hey.com>
2023-05-15 18:43:00 +1000

    prepare for 1.3.0 release (#59)

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //! The producer of a [`ValueBag`] may use a different strategy for capturing than the eventual
 //! consumer. They don't need to coordinate directly.
 
-#![doc(html_root_url = "https://docs.rs/value-bag/1.2.0")]
+#![doc(html_root_url = "https://docs.rs/value-bag/1.3.0")]
 #![no_std]
 
 /*
